d39b41ecd907724139885fdec8a563d4b9077866
Наталья Осипова <hypolactasia@gmail.com>
2017-11-22 10:57:25 +0300

    Правки

--- js/stat.js
+++ js/stat.js
@@ -1,42 +1,47 @@
 'use strict';
 
-window.renderStatistics = function (ctx, names, times) {
-  // облако
-  cloudShow(ctx);
-  // нахождение рез-тов времени
+// нахождение максимального рез-та времени
+var findMax = function (times) {
   var max = -1;
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-    }
-  }
+  times.forEach(function (i) {
+    max = Math.max(i, max);
+  });
+  return max;
+};
+
+// рандомное значение прозрачности цвета колонки
+var getRandomValue = function (minValue, maxValue) {
+  return Math.random() * (maxValue - minValue) + minValue;
+};
+
+window.renderStatistics = function (ctx, names, times) {
+
+  // облако
+  showCloud(ctx);
 
   // параметры колонок в гистограмме
-  for (i = 0; i < times.length; i++) {
-    var HISTOGRAM_HEIGHT = 88;
-    var STEP = HISTOGRAM_HEIGHT / (max - 0);
-    var BAR_WIDTH = 40;
-    var BAR_INDENT = 50;
-    var INITIAL_X = 118;
-    var INITIAL_Y = 88;
-    var LINE_HEIGHT = 20;
+  var HISTOGRAM_HEIGHT = 88;
+  var step = HISTOGRAM_HEIGHT / (findMax(times) - 0);
+  var BAR_WIDTH = 40;
+  var BAR_INDENT = 50;
+  var INITIAL_X = 118;
+  var INITIAL_Y = 88;
+  var LINE_HEIGHT = 20;
 
-    // рандомное значение прозрачности цвета колонки
-    var getRandomValue = function (minValue, maxValue) {
-      return Math.random() * (maxValue - minValue) + minValue;
-    };
-
+  for (var i = 0; i < times.length; i++) {
     // рисуем гистограмму
-    ctx.fillRect(INITIAL_X + (BAR_WIDTH + BAR_INDENT) * i, HISTOGRAM_HEIGHT + INITIAL_Y - times[i] * STEP, BAR_WIDTH, times[i] * STEP);
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(Math.round(times[i]), INITIAL_X + (BAR_WIDTH + BAR_INDENT) * i, HISTOGRAM_HEIGHT + INITIAL_Y - times[i] * STEP - LINE_HEIGHT / 2);
-    ctx.fillText(names[i], INITIAL_X + (BAR_WIDTH + BAR_INDENT) * i, INITIAL_Y + HISTOGRAM_HEIGHT + LINE_HEIGHT);
     ctx.fillStyle = (names[i] === 'Вы') ? 'rgba(0, 0, 0, 1.0)' : 'rgba(0, 0, 255, ' + getRandomValue(0.1, 1) + ')';
+    ctx.fillRect(INITIAL_X + (BAR_WIDTH + BAR_INDENT) * i, HISTOGRAM_HEIGHT + INITIAL_Y - times[i] * step, BAR_WIDTH, times[i] * step);
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.fillText(Math.round(times[i]), INITIAL_X + (BAR_WIDTH + BAR_INDENT) * i, HISTOGRAM_HEIGHT + INITIAL_Y - times[i] * step - LINE_HEIGHT / 2);
+    ctx.fillText(names[i], INITIAL_X + (BAR_WIDTH + BAR_INDENT) * i, INITIAL_Y + HISTOGRAM_HEIGHT + LINE_HEIGHT);
   }
 };
 
-var cloudShow = function (ctx) {
+var showCloud = function (ctx) {
+  var FIRST_TEXT = 'Ура, вы победили!';
+  var SECOND_TEXT = 'Список результатов:';
+
   ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
   ctx.lineWidth = '10';
   ctx.strokeStyle = 'rgba(220,220,220,0.8)';
@@ -63,7 +68,7 @@ var cloudShow = function (ctx) {
   ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
   ctx.font = '16px PT Mono';
 
-  ctx.fillText('Ура, вы победили!', 125, 40);
-  ctx.fillText('Список результатов:', 125, 60);
+  ctx.fillText(FIRST_TEXT, 125, 40);
+  ctx.fillText(SECOND_TEXT, 125, 60);
 };
 
